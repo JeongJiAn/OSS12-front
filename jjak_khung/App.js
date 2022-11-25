1088ed6a2f525e532b8ba532fa273105cae0ac0a
hyemimi <hyemi7375@gmail.com>
2022-11-25 17:02:33 +0900

    fix

--- jjak_khung/App.js
+++ jjak_khung/App.js
@@ -1,15 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Signin from './src/signin/signin';
 import Signup from './src/signup/signup';
+import Termtime from './src/termtime/termtime';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   const serverUrl = 'http://localhost:8080/';
-  const entryDeadline = { m: 9, d: 7 };
+  const entryDeadline = {m: 9, d: 7};
   const [semTime, setSemTime] = useState('');
   const [userId, setUserId] = useState('');
   const [userPassword, setUserPassword] = useState('');
@@ -23,34 +24,54 @@ const App = () => {
     const nowTime = new Date();
     const month = nowTime.getMonth() + 1;
     const date = nowTime.getDate();
-    if (month < entryDeadline.m || (month === entryDeadline.m && date <= entryDeadline.d)) {
+    if (
+      month < entryDeadline.m ||
+      (month === entryDeadline.m && date <= entryDeadline.d)
+    ) {
       setSemTime('entrytime');
     } else {
       setSemTime('termtime');
     }
   }, []);
 
-  function UserSignin (user) {
+  function UserSignin(user) {
     setUserId({id: user.user_id});
     setUserPassword({password: user.user_password});
-    setUserInfo({ ...userInfo, user_name: user.user_name, user_point: user.user_point, user_recom: user.user_recom, user_number: user.user_number });
-  };
-  
+    setUserInfo({
+      ...userInfo,
+      user_name: user.user_name,
+      user_point: user.user_point,
+      user_recom: user.user_recom,
+      user_number: user.user_number,
+    });
+  }
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='signin'>
-        <Stack.Screen name='signin' options={{headerShown: false}}>
-          {props => <Signin serverUrl={serverUrl} semTime={semTime} UserSignin={UserSignin} />}
+      <Stack.Navigator initialRouteName="signin">
+        <Stack.Screen name="signin" options={{headerShown: false}}>
+          {props => (
+            <Signin
+              serverUrl={serverUrl}
+              semTime={semTime}
+              UserSignin={UserSignin}
+            />
+          )}
         </Stack.Screen>
-        <Stack.Screen name='signup'>
+        <Stack.Screen name="signup">
           {props => <Signup serverUrl={serverUrl} />}
+        </Stack.Screen>
+        <Stack.Screen name="termtime" options={{headerShown: false}}>
+          {props => <Termtime userInfo={userInfo} serverUrl={serverUrl} />}
+        </Stack.Screen>
+        <Stack.Screen name="chatroom" options={{headerShown: false}}>
+          {props => <Chatroom userInfo={userInfo} serverUrl={serverUrl} />}
         </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
-  )
+  );
 };
 
-const styles = StyleSheet.create({
-});
+const styles = StyleSheet.create({});
 
 export default App;
--- jjak_khung/src/signin/signin.js
+++ jjak_khung/src/signin/signin.js
@@ -1,211 +1,223 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigation } from '@react-navigation/native';
-import { StyleSheet, View, Image, Text, TextInput, TouchableOpacity } from 'react-native';
-import { useIsFocused } from '@react-navigation/native';
+import React, {useState, useEffect} from 'react';
+import {useNavigation} from '@react-navigation/native';
+import {
+  StyleSheet,
+  View,
+  Image,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import {useIsFocused} from '@react-navigation/native';
 import applogo from '../../assets/image/logo-04.png';
 
-function Signin (props) {
-    const serverUrl = props.serverUrl;
-    const navigation = useNavigation();
-    const [signinFailText, setSigninFailText] = useState('');
-    const isFocused = useIsFocused();
-    let id = '';
-    let pw = '';
-    let loginResponse = false;
-    let userInfoResponse = { roleList: [], user_id: "", user_name: "", user_number: -1, user_password: "", user_point: -1, user_recom: -1, user_roles: "" };
-    let signinResult = false;
+function Signin(props) {
+  const serverUrl = props.serverUrl;
+  const navigation = useNavigation();
+  const [signinFailText, setSigninFailText] = useState('');
+  const isFocused = useIsFocused();
+  let id = '';
+  let pw = '';
+  let loginResponse = false;
+  let userInfoResponse = {
+    roleList: [],
+    user_id: '',
+    user_name: '',
+    user_number: -1,
+    user_password: '',
+    user_point: -1,
+    user_recom: -1,
+    user_roles: '',
+  };
+  let signinResult = false;
 
-    useEffect(() => {
-        setSigninFailText('');
-    }, [isFocused])
+  useEffect(() => {
+    setSigninFailText('');
+  }, [isFocused]);
 
-    const postSigninApi = async () => {
-        try {
-            const callUrl = serverUrl + 'login';
-            const postSigninResponse = await fetch(callUrl , {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    "user_id" : id,
-                    "user_password" : pw
-                })
-            });
-            if (!postSigninResponse.ok) {
-                throw new Error('400 or 500 error occurred');
-            }
-            loginResponse = true;
-            console.log('signin API Success');
-        } catch (error) {
-            loginResponse = false;
-            console.log(error + ' - signin API Fail');
-        }
-    };
-
-    const getUserInfoApi = async () => {
-        try {
-            const callUrl = props.serverUrl + 'info?id=' + id;
-            const infoResponse = await fetch(callUrl);
-            const infoJson = await infoResponse.json();
-            userInfoResponse = infoJson;
-            console.log('getUserInfo API Success');
-        } catch(e) {
-            console.log(e + ' - getUserInfo API Fail');
-        }
-    };
-
-    const trySignIn = async () => {
-        await postSigninApi();
-        if (loginResponse === true) {
-            await getUserInfoApi();
-            const userInfo = userInfoResponse;
-            await props.UserSignin(userInfo);
-            if (props.semTime === 'entrytime'){
-                navigation.navigate('entrytime');
-            } else if (props.semTime === 'termtime') {
-                navigation.navigate('termtime');
-            }
-        } else {
-            signinResult = false;
-            alert('로그인 실패');
-            setSigninFailText('아이디 또는 비밀번호가 일치하지 않습니다.');
-        }
-    };
-
-    function IdInput () {
-        return (
-            <TextInput
-                placeholder='ID'
-                style={styles.idInput}
-                multiline={false}
-                maxLength={16}
-                autoCapitalize={'none'}
-                editable={true}
-                onChangeText={(text) => id = text}
-            />
-        );
-    };
-
-    function PasswordInput () {
-        return(
-            <TextInput
-                placeholder='🔑Password'
-                style={styles.passwordInput}
-                multiline={false}
-                maxLength={16}
-                autoCapitalize={'none'}
-                editable={true}
-                onChangeText={(text) => pw = text}
-                secureTextEntry={true}
-            />
-        );
-    };
-
-    function SigninFailText () {
-        return(
-            <View style={{marginLeft: '10%', width: 300, padding: 10}}>
-                <Text style={{color: '#ff0000'}}>{signinFailText}</Text>
-            </View>
-        );
-    };
-
-    function SigninButton () {
-        return (
-            <TouchableOpacity
-                style={styles.signinButton}
-                onPress={() => trySignIn()}
-            >
-                <Text style={{color: '#ffffff', fontSize: 20}}>로그인</Text>
-            </TouchableOpacity>
-        );
-    };
-
-    function SignupButton (){
-        return(
-            <TouchableOpacity
-                style={styles.signupButton}
-                onPress={() => navigation.navigate('signup')}
-            >
-                <Text style={styles.signupBoxText}>회원가입</Text>
-            </TouchableOpacity>
-        )
+  const postSigninApi = async () => {
+    try {
+      const callUrl = serverUrl + 'login';
+      const postSigninResponse = await fetch(callUrl, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          user_id: id,
+          user_password: pw,
+        }),
+      });
+      if (!postSigninResponse.ok) {
+        throw new Error('400 or 500 error occurred');
+      }
+      loginResponse = true;
+      console.log('signin API Success');
+    } catch (error) {
+      loginResponse = false;
+      console.log(error + ' - signin API Fail');
     }
+  };
 
-    return(
-        <View style={styles.signinWrap}>
-            <Image
-                style={styles.logo}
-                // resizeMode='contain'
-                source={applogo}
-                />
-            <IdInput/>
-            <PasswordInput/>
-            <SigninFailText/>
-            <SigninButton/>
-            <SignupButton/>
-        </View>
-    )
-};
+  const getUserInfoApi = async () => {
+    try {
+      const callUrl = props.serverUrl + 'info?id=' + id;
+      const infoResponse = await fetch(callUrl);
+      const infoJson = await infoResponse.json();
+      userInfoResponse = infoJson;
+      console.log('getUserInfo API Success');
+    } catch (e) {
+      console.log(e + ' - getUserInfo API Fail');
+    }
+  };
+
+  const trySignIn = async () => {
+    await postSigninApi();
+    if (loginResponse === true) {
+      await getUserInfoApi();
+      const userInfo = userInfoResponse;
+      await props.UserSignin(userInfo);
+      if (props.semTime === 'entrytime') {
+        navigation.navigate('entrytime');
+      } else if (props.semTime === 'termtime') {
+        navigation.navigate('termtime');
+      }
+    } else {
+      signinResult = false;
+      alert('로그인 실패');
+      setSigninFailText('아이디 또는 비밀번호가 일치하지 않습니다.');
+    }
+  };
+
+  function IdInput() {
+    return (
+      <TextInput
+        placeholder="ID"
+        style={styles.idInput}
+        multiline={false}
+        maxLength={20}
+        autoCapitalize={'none'}
+        editable={true}
+        onChangeText={text => (id = text)}
+      />
+    );
+  }
+
+  function PasswordInput() {
+    return (
+      <TextInput
+        placeholder="🔑Password"
+        style={styles.passwordInput}
+        multiline={false}
+        maxLength={16}
+        autoCapitalize={'none'}
+        editable={true}
+        onChangeText={text => (pw = text)}
+        secureTextEntry={true}
+      />
+    );
+  }
+
+  function SigninFailText() {
+    return (
+      <View style={{marginLeft: '10%', width: 300, padding: 10}}>
+        <Text style={{color: '#ff0000'}}>{signinFailText}</Text>
+      </View>
+    );
+  }
+
+  function SigninButton() {
+    return (
+      <TouchableOpacity style={styles.signinButton} onPress={() => trySignIn()}>
+        <Text style={{color: '#ffffff', fontSize: 20}}>로그인</Text>
+      </TouchableOpacity>
+    );
+  }
+
+  function SignupButton() {
+    return (
+      <TouchableOpacity
+        style={styles.signupButton}
+        onPress={() => navigation.navigate('signup')}>
+        <Text style={styles.signupBoxText}>회원가입</Text>
+      </TouchableOpacity>
+    );
+  }
+
+  return (
+    <View style={styles.signinWrap}>
+      <Image
+        style={styles.logo}
+        // resizeMode='contain'
+        source={applogo}
+      />
+      <IdInput />
+      <PasswordInput />
+      <SigninFailText />
+      <SigninButton />
+      <SignupButton />
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
-    signinWrap: {
-        width: '100%',
-        height: '100%',
-        backgroundColor: '#F8F9FF'
-    },
-    logo: {
-        width: '100%',
-        height: 280
-    },
-    idInput: {
-        width: '80%',
-        height: 48,
-        marginLeft: '10%',
-        borderWidth:1,
-        borderColor:'#E4E4E4',
-        borderRadius: 6,
-        backgroundColor:'#ffffff',
-        paddingLeft:14,
-        justifyContent:'center',
-        fontSize: 14,
-    },
-    passwordInput: {
-        width: '80%',
-        height: 48,
-        marginTop: 30,
-        marginLeft: '10%',
-        borderWidth:1,
-        borderColor:'#E4E4E4',
-        borderRadius: 6,
-        backgroundColor:'#ffffff',
-        paddingLeft:14,
-        justifyContent:'center',
-        fontSize: 14,
-    },
-    signinButton: {
-        alignItems: 'center',
-        width: '80%',
-        height: 48,
-        marginTop: '10%',
-        marginLeft: '10%',
-        paddingTop:16,
-        borderRadius:6,
-        backgroundColor: '#7173c9',
-    },
-    signupButton: {
-        alignItems: 'center',
-        justifyContent:'center',
-        width: '80%',
-        height: 48,
-        marginTop: 20,
-        marginLeft: '10%',
-        borderRadius:6,
-        borderWidth:1,
-        borderColor:'#7173c9',
-        backgroundColor: '#FFFFFF',
-    }
+  signinWrap: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#F8F9FF',
+  },
+  logo: {
+    width: '100%',
+    height: 280,
+  },
+  idInput: {
+    width: '80%',
+    height: 48,
+    marginLeft: '10%',
+    borderWidth: 1,
+    borderColor: '#E4E4E4',
+    borderRadius: 6,
+    backgroundColor: '#ffffff',
+    paddingLeft: 14,
+    justifyContent: 'center',
+    fontSize: 14,
+  },
+  passwordInput: {
+    width: '80%',
+    height: 48,
+    marginTop: 30,
+    marginLeft: '10%',
+    borderWidth: 1,
+    borderColor: '#E4E4E4',
+    borderRadius: 6,
+    backgroundColor: '#ffffff',
+    paddingLeft: 14,
+    justifyContent: 'center',
+    fontSize: 14,
+  },
+  signinButton: {
+    alignItems: 'center',
+    width: '80%',
+    height: 48,
+    marginTop: '10%',
+    marginLeft: '10%',
+    paddingTop: 16,
+    borderRadius: 6,
+    backgroundColor: '#7173c9',
+  },
+  signupButton: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '80%',
+    height: 48,
+    marginTop: 20,
+    marginLeft: '10%',
+    borderRadius: 6,
+    borderWidth: 1,
+    borderColor: '#7173c9',
+    backgroundColor: '#FFFFFF',
+  },
 });
 
 export default Signin;
